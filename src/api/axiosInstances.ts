17aa864648db15575a26ade4d940022a756d05c4
Codeeq <errytage@gmail.com>
2025-01-31 09:43:56 +0100

    add base url

--- src/api/axiosInstances.ts
+++ src/api/axiosInstances.ts
@@ -1,16 +1,17 @@
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import Cookies from "js-cookie";
+import axios from "axios";
 
-const baseUrl = 'https://tbt-agro.whooshing.xyz/api/v1';
+const baseUrl = "https://api.tbt.live.whooshing.xyz/api/v1";
+// const baseUrl = 'https://tbt-agro.whooshing.xyz/api/v1';
 
 const baseAPI = axios.create({ baseURL: baseUrl });
 
 baseAPI.interceptors.request.use(async (config) => {
-  const userToken = Cookies.get('whooshNgToken');
+  const userToken = Cookies.get("whooshNgToken");
   // If userToken exists, set 'Authorization' and 'x-access-token' headers to the user token
   if (userToken) {
-    config.headers['Authorization'] = `Bearer ${userToken}`;
-    config.headers['x-access-token'] = userToken;
+    config.headers["Authorization"] = `Bearer ${userToken}`;
+    config.headers["x-access-token"] = userToken;
   }
 
   return config;
